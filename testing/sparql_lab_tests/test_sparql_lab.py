e69bcce74e771660d16e32d889a13cd050c5e66f
mia.bajic <mia.bajic@morosystems.cz>
2020-11-15 16:46:34 +0100

    adding tests

--- testing/sparql_lab_tests/test_sparql_lab.py
+++ testing/sparql_lab_tests/test_sparql_lab.py
@@ -11,11 +11,23 @@ class WebChecker(unittest.TestCase):
         self.driver = webdriver.Chrome(r'C:\Users\mbajic\Documents\python\testing\sparql_lab_tests\chromedriver.exe')
 
     # functions checks if page is running
-    def test1_page_running(self):
+    def test_page_running(self):
         driver = self.driver
         driver.get("https://doc.lmcloud.vse.cz/sparqlab")
         title = driver.find_element_by_tag_name("h1")
         assert 'Exercises by difficulty' in title.text
+        # self.assertIn("Exercises by difficulty", title.text)
+
+    # function checks, if there are correct elements in the navigation bar
+    def test_navigation_bar(self):
+        driver = self.driver
+        driver.get("https://doc.lmcloud.vse.cz/sparqlab")
+        nav_bar = driver.find_element_by_id("collapsing-navbar")
+        assert "SPARQLabβ" in nav_bar.text
+        assert "Exercises" in nav_bar.text
+        assert "SPARQL endpoint" in nav_bar.text
+        assert "Data" in nav_bar.text
+        assert "About" in nav_bar.text
 
     def tearDown(self):
         self.driver.close()
