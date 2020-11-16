5789e36c0dabdcaef8959a39a0ac38caf4ff5d05
mia.bajic <mia.bajic@morosystems.cz>
2020-11-16 18:46:26 +0100

    adding test 'show some data'

--- testing/sparql_lab_tests/test_sparql_lab.py
+++ testing/sparql_lab_tests/test_sparql_lab.py
@@ -1,8 +1,11 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common import action_chains
+from selenium.webdriver.common.keys import Keys
 
 
 class WebChecker(unittest.TestCase):
@@ -18,7 +21,7 @@ class WebChecker(unittest.TestCase):
         assert 'Exercises by difficulty' in title.text
         # self.assertIn("Exercises by difficulty", title.text)
 
-    # function checks, if there are correct elements in the navigation bar
+    # function checks if there are correct elements in the navigation bar
     def test_navigation_bar(self):
         driver = self.driver
         driver.get("https://doc.lmcloud.vse.cz/sparqlab")
@@ -29,8 +32,22 @@ class WebChecker(unittest.TestCase):
         assert "Data" in nav_bar.text
         assert "About" in nav_bar.text
 
+    # exercise 'show some data' given correct query
+    def test_show_data(self):
+        driver = self.driver
+        driver.get("https://doc.lmcloud.vse.cz/sparqlab/exercise/show/some-data")
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CLASS_NAME, "CodeMirror-scroll")))
+        editor = driver.find_element_by_css_selector('.CodeMirror  textarea')
+        editor.send_keys('ASK { \n [] ?p [] . \n }')
+        send_button = driver.find_element_by_xpath('//button[text()="Submit"]')
+        send_button.click()
+        driver.implicitly_wait(20)
+        checkmark = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div/h2/i')
+        assert checkmark.is_displayed()
+
     def tearDown(self):
-        self.driver.close()
+        time.sleep(4)
+        # self.driver.close()
 
 
 if __name__ == "__main__":
