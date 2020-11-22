ab26f4b575c86b61c0c29a9b43a314b6d9e702a6
mia.bajic <mia.bajic@morosystems.cz>
2020-11-22 23:36:07 +0100

    adding test data

--- testing/sparql_lab_tests/test_sparql_lab.py
+++ testing/sparql_lab_tests/test_sparql_lab.py
@@ -24,12 +24,45 @@ class CorrectQueriesChecker(unittest.TestCase):
             # print("query is: ", query)
             self.query_checker_helper(url, query)
 
-    test_cases_dict = {1: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/describe-dimensions",
+    test_cases_dict = {1: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/some-data",
                            'PREFIX qb: <http://purl.org/linked-data/cube#> \n'
                            'DESCRIBE ?dimension \n WHERE { \n   ?dimension a qb:DimensionProperty . \n } '],
-                       2: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/classes-histogram",
-                           'SELECT ?class (COUNT(?s) AS ?count) \n WHERE { \n ?s a ?class . \n } '
-                           '\n GROUP BY ?class \n ORDER BY DESC(?count)']}
+                       2: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/one-triple",
+                           'SELECT * \n WHERE { \n ?s ?p ?o . \n } LIMIT 1 '],
+                       3: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/some-named-graphs",
+                           'ASK \n WHERE { \n GRAPH ?g { \n [] ?p [] . \n } \n }'],
+                       4: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/classes",
+                           'SELECT DISTINCT ?class \n WHERE { \n [] a ?class . \n }'],
+                       5: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/count-triples",
+                           'SELECT (COUNT(*) as ?count) \n WHERE { \n ?s ?p ?o . \n }'],
+                       6: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/properties",
+                           'SELECT DISTINCT ?p \n WHERE { \n [] ?p [] . \n } '],
+                       7: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/describe-orphan-pension",
+                           'DESCRIBE <https://data.cssz.cz/resource/pension-kind/PK_D>'],
+                       8: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/one-quad",
+                           'SELECT * \n WHERE { \n GRAPH ?g { \n ?s ?p ?o . \n } \n } \n LIMIT 1'],
+                       9: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/named-graphs",
+                           'SELECT DISTINCT ?g \n WHERE { \n GRAPH ?g { \n ?s ?p ?o . \n } \n }'],
+                       10: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/classes-histogram",
+                            'SELECT ?class (COUNT(?s) AS ?count) \n WHERE { \n ?s a ?class . \n } '
+                            '\n GROUP BY ?class \n ORDER BY DESC(?count)'],
+                       11: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/properties-histogram",
+                            'SELECT ?p (COUNT(*) as ?count) \n WHERE { \n [] ?p [] . \n } \n '
+                            'GROUP BY ?p \n ORDER BY DESC( ?count)'],
+                       12: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/dataset-equals-named-graph",
+                            'PREFIX qb: <http://purl.org/linked-data/cube#> \n ASK \n WHERE { \n GRAPH ?d { \n '
+                            '?d a qb:DataSet . \n } \n }'],
+                       13: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/understanding-from",
+                            'SELECT (COUNT(*) AS ?count) \n FROM '
+                            '<https://data.cssz.cz/resource/dataset/rocenka/vocabulary> \n'
+                            ' WHERE { \n ?s ?p ?o . \n }'],
+                       14: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/describe-dimensions",
+                            'PREFIX qb: <http://purl.org/linked-data/cube#> \n DESCRIBE ?dimension \n WHERE { \n '
+                            '?dimension a qb:DimensionProperty . \n }'],
+                       15: ["https://doc.lmcloud.vse.cz/sparqlab/exercise/show/graph-sizes",
+                            'SELECT ?g (COUNT(*) as ?count) \n WHERE { \n GRAPH ?g { \n [] ?p [] . \n } \n } \n'
+                            ' GROUP BY ?g \n ORDER BY DESC(?count)'],
+                       }
 
     def query_checker_helper(self, url, query):
         self.driver.get(url)
@@ -45,8 +78,8 @@ class CorrectQueriesChecker(unittest.TestCase):
         assert checkmark.is_displayed()
 
     def tearDown(self):
-        time.sleep(60)
-        # self.driver.close()
+        # time.sleep(60)
+        self.driver.close()
 
 
 CorrectQueriesChecker()
