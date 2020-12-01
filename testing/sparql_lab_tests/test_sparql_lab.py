b28f38c0bf23c9e8a8f2ff01e35ca11badfc73c8
mia.bajic <mia.bajic@morosystems.cz>
2020-12-01 23:19:10 +0100

    adding test data

--- testing/sparql_lab_tests/test_sparql_lab.py
+++ testing/sparql_lab_tests/test_sparql_lab.py
@@ -8,44 +8,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from data_correct_queries_checker import test_cases_dict
 
 
-class CorrectQueriesChecker(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"C:\Users\mbajic\Documents\python\testing\sparql_lab_tests"
-                                                       r"\chromedriver.exe")
-
-    def test_load_test_cases(self):
-        global url
-        global query
-        for test_case, url_and_query in test_cases_dict.items():
-            with self.subTest(test_case=test_case, url_and_query=url_and_query):
-                url = url_and_query[0]
-                query = url_and_query[1]
-                self.driver.get(url)
-                WebDriverWait(self.driver, 60).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "CodeMirror-scroll")))
-                editor = self.driver.find_element_by_css_selector('.CodeMirror  textarea')
-                editor.send_keys(Keys.CONTROL + "a")
-                editor.send_keys(Keys.DELETE)
-                editor.send_keys(query)
-                send_button = self.driver.find_element_by_xpath('//button[text()="Submit"]')
-                send_button.click()
-                self.driver.implicitly_wait(20)
-                checkmark = self.driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div/h2/i')
-                assert checkmark.is_displayed()
-
-    def tearDown(self):
-        # time.sleep(60)
-        self.driver.close()
-
-
-CorrectQueriesChecker()
+# class CorrectQueriesChecker(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.driver = webdriver.Chrome(executable_path=r'C:/Users/mbajic/Documents/python/testing/sparql_lab_tests'
+#                                                           r'/chromedriver.exe')
+#
+#     def test_load_test_cases(self):
+#         global url
+#         global query
+#         for test_case, url_and_query in test_cases_dict.items():
+#             with self.subTest(test_case=test_case, url_and_query=url_and_query):
+#                 url = url_and_query[0]
+#                 query = url_and_query[1]
+#                 self.driver.get(url)
+#                 WebDriverWait(self.driver, 60).until(
+#                     EC.presence_of_element_located((By.CLASS_NAME, "CodeMirror-scroll")))
+#                 editor = self.driver.find_element_by_css_selector('.CodeMirror  textarea')
+#                 editor.send_keys(Keys.CONTROL + "a")
+#                 editor.send_keys(Keys.DELETE)
+#                 editor.send_keys(query)
+#                 send_button = self.driver.find_element_by_xpath('//button[text()="Submit"]')
+#                 send_button.click()
+#                 self.driver.implicitly_wait(20)
+#                 checkmark = self.driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div/h2/i')
+#                 assert checkmark.is_displayed()
+#
+#     def tearDown(self):
+#         # time.sleep(60)
+#         self.driver.close()
+#
+#
+# CorrectQueriesChecker()
 
 
 class UiElementsChecker(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"C:\Users\mbajic\Documents\python\testing\sparql_lab_tests"
-                                                       r"\chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path=r'C:/Users/mbajic/Documents/python/testing/sparql_lab_tests'
+                                                       r'/chromedriver.exe')
 
     def test_page_running(self):
         driver = self.driver
@@ -63,6 +63,17 @@ class UiElementsChecker(unittest.TestCase):
         assert "SPARQL endpoint" in nav_bar.text
         assert "Data" in nav_bar.text
         assert "About" in nav_bar.text
+
+    def test_search_function(self):
+        self.driver = self.driver
+        self.driver.get("https://doc.lmcloud.vse.cz/sparqlab")
+        search = self.driver.find_element_by_xpath('//*[@id="search-term"]')
+        search.click()
+        search.send_keys("datatype")
+        datatype_menu = self.driver.find_element_by_xpath('//*[@id="collapsing-navbar"]/form/div/div/div/ul/li/mark')
+        datatype_menu.click()
+        found_exercise = self.driver.find_element_by_xpath('/html/body/div/ul')
+        assert "Datové typy" in found_exercise.text
 
     def tearDown(self):
         # time.sleep(60)
