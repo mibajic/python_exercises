d67ec3469a059a34807dcd088297ca312b5e3dd5
mia.bajic <mia.bajic@morosystems.cz>
2021-01-12 23:41:32 +0100

    adding exercises

---
+++ core_python/data_structures_course/exercises.py
@@ -0,0 +1,58 @@
+def sum_list():
+    items_list = [1, 2, 3, 4, 5]
+    sum_numbers = 0
+    for i in items_list:
+        sum_numbers += i
+    return sum_numbers
+
+
+print(sum_list())
+
+
+def multiply_list():
+    items_list = [1, 2, 3]
+    multiply = 1
+    for i in items_list:
+        multiply *= i
+    return multiply
+
+
+print(multiply_list())
+
+largest_number_list = [7, -2, 89, 2, 4]
+print(max(largest_number_list))
+
+
+def match_words(words):
+    string = 0
+
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            string += 1
+    return string
+
+
+my_list = ['abc', 'xyz', 'aba', '1221', 'ab']
+print(match_words(my_list))
+
+unsorted_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+sorted_list = sorted(unsorted_list, key=lambda x: x[1])
+print(sorted_list)
+
+
+def without(duplicate_list):
+    list_without_duplicates = []
+    for i in duplicate_list:
+        if i not in list_without_duplicates:
+            list_without_duplicates.append(i)
+    return list_without_duplicates
+
+
+list_with_duplicates = [1, 1, 2, 3, 4, 4, 5]
+print(without(list_with_duplicates))
+
+my_list = []
+if not my_list:
+    print("Empty")
+else:
+    print("Not empty")
