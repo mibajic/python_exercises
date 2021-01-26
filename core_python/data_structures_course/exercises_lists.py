64acdad8383ab77b4a2119e5308df0212228317a
mia.bajic <mia.bajic@morosystems.cz>
2021-01-26 12:14:40 +0100

    adding course lessons

--- core_python/data_structures_course/exercises_lists.py
+++ core_python/data_structures_course/exercises_lists.py
@@ -1,19 +1,19 @@
-# listOne = [3, 6, 9, 12, 15, 18, 21]
-# listTwo = [4, 8, 12, 16, 20, 24, 28]
-#
-# oddElements = listOne[1::2]
-# print("Elements at odd-index positions from list one")
-# print(oddElements)
-#
-# evenElements = listTwo[0::2]
-# print("Elements at even-index positions from list two")
-# print(evenElements)
+listOne = [3, 6, 9, 12, 15, 18, 21]
+listTwo = [4, 8, 12, 16, 20, 24, 28]
 
-# List = [54, 44, 27, 79, 91, 41]
-#
-# element = List.pop(4)
-# List.insert(2, element)
-# List.append(element)
+oddElements = listOne[1::2]
+print("Elements at odd-index positions from list one")
+print(oddElements)
+
+evenElements = listTwo[0::2]
+print("Elements at even-index positions from list two")
+print(evenElements)
+
+List = [54, 44, 27, 79, 91, 41]
+
+element = List.pop(4)
+List.insert(2, element)
+List.append(element)
 
 List = [11, 45, 8, 23, 14, 12, 78, 45, 89]
 print(List[:3])
@@ -22,3 +22,11 @@ chunk3 = List[6:9]
 print(chunk3)
 chunk3.reverse()
 print(chunk3)
+
+list1 = [2, 4, 6]
+list2 = [1, 3, 5]
+
+for i in list1:
+    list2.append(i)
+print(list2)
+
